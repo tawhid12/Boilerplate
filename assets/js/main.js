e53e3ae88fafe92658323ac625918d02af4fdb2e
tawhid12 <tawhid99@gmail.com>
2020-05-16 22:50:16 +0600

    Business template initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,14 @@ $(document).ready(function($){
 
 	// ---- Active
     $(".owl").owlCarousel();
+	$('.slider-area').owlCarousel({
+			items:1,
+			loop:true,
+			margin:10,
+			nav:false,
+			dots:true,
+			autoplay:false
+	});	
     // Owl Next Privew Change
     //$( ".owl-prev").html('<i class="fa screenshort-arow fa-chevron-left"></i>');
     //$( ".owl-next").html('<i class="fa screenshort-arow fa-chevron-right"></i>');
